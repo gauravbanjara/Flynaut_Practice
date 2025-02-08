27f63b8decfb56d4da8b49d6e65d713d325ebc3e
gauravbanjara <jadhavgaurav377@gmail.com>
2025-02-08 10:16:50 +0530

    add in ArrayList

--- Collection/ArrayListExample.java
+++ Collection/ArrayListExample.java
@@ -13,7 +13,7 @@ public class ArrayListExample {
         list.add("three");
         list.add("five");
         list.add(null); // null values are allowed
-        list.set(1,"gaurav");
+        list.set(1,"Gaurav");
         System.out.println(list);
 
 //       access the element using index
