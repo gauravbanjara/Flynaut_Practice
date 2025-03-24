d573d1d1cd1ff9c812b5fe1cf95ef82b7f58842a
gauravbanjara <jadhavgaurav377@gmail.com>
2025-03-24 22:27:32 +0530

    add new files

--- IJP/FirstNonRepeatingChar.java
+++ IJP/FirstNonRepeatingChar.java
@@ -1,21 +1,40 @@
 package IJP;
 
+import java.util.HashMap;
+
 public class FirstNonRepeatingChar {
     public static void main(String[] args) {
-        String str = "gaurav";
-        char [] arr = str.toCharArray();
-        for (int i = 0; i <= arr.length-2 ; i++) {
-            int count = 0;
-            for (int j = i+1; j <= arr.length-1 ; j++) {
-                if (arr[i] == arr[j]){
-                    count++;
-                }
-            }
-            if(count==0)
-            {
-                System.out.println(arr[i]);
+        String str = "aahhaaggtbhau";
+//        char [] arr = str.toCharArray();
+//        for (int i = 0; i <= arr.length-2 ; i++) {
+//            int count = 0;
+//            for (int j = i+1; j <= arr.length-1 ; j++) {
+//                if (arr[i] == arr[j]){
+//                    count++;
+//                }
+//            }
+//            if(count==0)
+//            {
+//                System.out.println(arr[i]);
+//                break;
+//            }
+//        }
+
+
+        //  using Hashmap
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        // Count the occurrences of each character
+        for (char ch : str.toCharArray()) {
+            count.put(ch, count.getOrDefault(ch, 0) + 1);
+        }
+        // Find the first character with a count of 1
+        for (char ch : str.toCharArray()) {
+            if (count.get(ch) == 1) {
+                System.out.println(ch);
                 break;
             }
         }
+
     }
 }
